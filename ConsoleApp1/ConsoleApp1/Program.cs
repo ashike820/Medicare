f5a31440157629c3cd29af4eaa1ba4566eb714c6
ashike820 <ashike820@gmail.com>
2021-03-09 01:23:53 +0600

    This is my first commit . UI design complete

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, a = 5;
+            string b = "5";
+            c = Convert.ToInt32(b);
+            int d = a + c;
+            Console.WriteLine("Hello World!" + d);
+        }
+    }
+}
